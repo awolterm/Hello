8300515f63b7127fe940465db328b3545e41e316
awolterm <59893971+awolterm@users.noreply.github.com>
2020-01-16 11:41:50 -0600

    Added Majors
    added code that displayed majors

---
+++ hello.c
@@ -0,0 +1,20 @@
+/* Hack 1 Nickalus Davis & Amy Wolterman
+CSE 155E Sec 150
+Date: 01/16/2020
+*/
+
+
+#include <stdio.h> //Preprocessor Directive
+
+//Main Function
+int main (void) {
+
+//Executable code that displays text
+	printf("Nickalus Davis & Amy Wolterman\n");
+
+//Executable code that displays Majors
+	printf("Mechanical Engineering & Mechanical Engineering\n");
+
+//End of function
+	return 0;
+}
